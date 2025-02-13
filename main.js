b86e387c6ab1c8a7af91dea4bffb144d9dd0e010
Mostafa <bttt8888444@gmail.com>
2025-02-13 18:58:20 +0300

    socket fix

--- main.js
+++ main.js
@@ -56,7 +56,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin:"*",
+        origin:"https://scholagram.com",
         methods: ["GET", "POST","DELETE","PUT"],
         
     }   
